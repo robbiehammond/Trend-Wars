ccf1d72752b27d300ae1b6e9a6d58ea96595188d
Andrew Gross <andrew144@sbcglobal.net>
2022-10-13 20:20:57 -0400

    make playerlist look nice

--- frontend/game/src/PlayerList/PlayerList.js
+++ frontend/game/src/PlayerList/PlayerList.js
@@ -7,40 +7,53 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Checkbox from '@mui/material/Checkbox';
 import Avatar from '@mui/material/Avatar';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 class PlayerList  extends React.Component{
+  constructor(props){
+    super(props);
+    // todo: make this what the server says the players are 
+    this.players = [
+      { name: 'Rob B'}, 
+      { name: 'Tyler Nish', submitted: true }, 
+      { name: 'Big Andy-rew', submitted: false }, 
+      { name: 'Your mom', submitted: false }, 
+      { name: 'Burger', submitted: true }]; 
+  }
 
   render(){
     return (
       <div className="playerList">
         <h2>Player List</h2>
-          <List dense sx={{}}>
-      {[0, 1, 2, 3].map((value) => {
-        const labelId = `checkbox-list-secondary-label-${value}`;
-        return (
-          <ListItem
-            key={value}
-            secondaryAction={
-              <Checkbox
-                edge="end"
-                inputProps={{ 'aria-labelledby': labelId }}
-              />
-            }
-            disablePadding
-          >
-            <ListItemButton>
-              <ListItemAvatar>
-                <Avatar
-                  alt={`Avatar n°${value + 1}`}
-                  src={`/static/images/avatar/${value + 1}.jpg`}
-                />
-              </ListItemAvatar>
-              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            </ListItemButton>
-          </ListItem>
-        );
-      })}
-    </List>
+        <List dense sx={{}}>
+          {this.players.map((player) => {
+            const labelId = `checkbox-list-secondary-label-${player}`;
+            return (
+              <ListItem
+                key={player}
+                disablePadding
+              >
+                <ListItemButton>
+                  <ListItemAvatar>
+                    <Avatar
+                      alt={`Avatar n°${player + 1}`}
+                      src={`/static/images/avatar/${player + 1}.jpg`}
+                    />
+                  </ListItemAvatar>
+                  <ListItemText
+                    id={labelId}
+                    primary={`${player.name} ${
+                      player.word ? "wrote " + player.word : ""
+                    }`}
+                  />
+                  <>{player.submitted ? 
+                  <CheckCircleIcon></CheckCircleIcon> : ''
+                }</>
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
       </div>
     );
   } 
