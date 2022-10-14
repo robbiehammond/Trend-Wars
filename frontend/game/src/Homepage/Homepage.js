4e5b1f9aec33d3c1d8d92c839a482ed519ad70db
Robbie Hammond <robbiehammond3@gmail.com>
2022-10-14 16:46:12 -0400

    added MessageType to frontend

--- frontend/game/src/Homepage/Homepage.js
+++ frontend/game/src/Homepage/Homepage.js
@@ -4,7 +4,8 @@ import { useNavigate }from 'react-router-dom';
 import ws from '../socketConfig.js';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import Message from '../Message';
+import Message from '../Message/Message';
+import MessageType from '../Message/MessageType';
 
 function Homepage()  {
   ws.on('message', (message) => {
@@ -13,28 +14,28 @@ function Homepage()  {
   const navigate = useNavigate();
 
   function sendCreateLobbyMessage() {
-    const msg = new Message('CREATE_LOBBY', {data: "test"});
+    const msg = new Message(MessageType.CREATE_LOBBY, {data: "test"});
     ws.emit('message', msg.toJSON());
   }
 
 
   function sendJoinLobbyMessage() {
-    const msg = new Message('PLAYER_JOIN', {data: {lobbyID: "AAAAAA"}});
+    const msg = new Message(MessageType.PLAYER_JOIN, {data: {lobbyID: "AAAAAA"}});
     ws.emit('message', msg.toJSON());
   }
 
   function sendReadyMsg() {
-    const msg = new Message('READY', {data: "meme"});
+    const msg = new Message(MessageType.READY, {data: "meme"});
     ws.emit('message', msg.toJSON());
   }
 
   function sendStartGameMsg() {
-    const msg = new Message('START_GAME', {data: "meme"});
+    const msg = new Message(MessageType.START_GAME, {data: "meme"});
     ws.emit('message', msg.toJSON());
   }
 
   function submitWordMsg() {
-    const msg = new Message('SUBMIT_WORD', {data: "meme", word: "test"});
+    const msg = new Message(MessageType.SUBMIT_WORD, {data: "meme", word: "test"});
     ws.emit('message', msg.toJSON());
   }
   return (
