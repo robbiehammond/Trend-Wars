4f7af5ffae18fc82215a219716e27f4ea3732a8b
Robbie Hammond <robbiehammond3@gmail.com>
2022-12-06 21:38:57 -0500

    username changing works in lobby

--- backend/Lobby.py
+++ backend/Lobby.py
@@ -60,6 +60,9 @@ class Lobby:
             # Basically, it means we don't need to do anything with the message, so just drop it.
             case "URL": 
                 pass
+            case "USERNAME":
+                player.username = message.msgData['data']
+                self.CM.send_to_all_in_lobby(self.id, Message(MessageType.LOBBY_STATE, self.getLobbyState()))
             case "RANDOMIZE_BIGHEAD":
                 player.randomizeBigHead()
                 self.CM.send_to_all_in_lobby(self.id, Message(MessageType.LOBBY_STATE, self.getLobbyState()))
