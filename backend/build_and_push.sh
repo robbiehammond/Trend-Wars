36f630f95e31a28f4faeb1539baa711e7b2fc460
Robbie Hammond <robbiehammond3@gmail.com>
2025-02-02 14:48:46 -0800

    add script to push new docker container

---
+++ backend/build_and_push.sh
@@ -0,0 +1,9 @@
+docker build -t trend-wars-backend .
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 592903510708.dkr.ecr.us-west-2.amazonaws.com
+docker push 592903510708.dkr.ecr.us-west-2.amazonaws.com/trend-wars-backend
+aws ecs update-service \
+  --cluster TrendWarsStack-TrendWarsClusterB24DE657-qNjZZGVb3DF4 \
+  --service TrendWarsStack-TrendWarsFargateServiceACB1E9D6-DhLRw7Tkyobh \
+  --force-new-deployment
+
+
