1e2f59dc2b56f449afec02f270bb1a920f0931cd
Robbie Hammond <robbiehammond3@gmail.com>
2025-02-02 15:53:26 -0800

    remove some turn timer related stuff

--- backend/build_and_push.sh
+++ backend/build_and_push.sh
@@ -1,5 +1,6 @@
 docker build -t trend-wars-backend .
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 592903510708.dkr.ecr.us-west-2.amazonaws.com
+docker tag trend-wars-backend 592903510708.dkr.ecr.us-west-2.amazonaws.com/trend-wars-backend
 docker push 592903510708.dkr.ecr.us-west-2.amazonaws.com/trend-wars-backend
 aws ecs update-service \
   --cluster TrendWarsStack-TrendWarsClusterB24DE657-qNjZZGVb3DF4 \
